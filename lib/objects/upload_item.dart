6b066b93564885282bc8ebf0a2333d928962cf78
Simon <reidst@hendrix.edu>
2023-07-24 15:41:21 -0500

    Added error messages for uploads

--- lib/objects/upload_item.dart
+++ lib/objects/upload_item.dart
@@ -1,102 +1,81 @@
-import 'package:flutter/material.dart' show debugPrint;
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:hendrix_today_uploader/objects/excel_data.dart';
 
-class UploadItem {
-  const UploadItem({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.type,
-    required this.contactName,
-    required this.contactEmail,
-    required this.beginPosting,
-    required this.endPosting,
-    this.date,
-    this.time,
-    this.location,
-    this.applyDeadline,
-  });
-  final int id;
-  final String title;
-  final String description;
-  final String type;
-  final String contactName;
-  final String contactEmail;
-  final DateTime beginPosting;
-  final DateTime endPosting;
-  final DateTime? date;
-  final String? time;
-  final String? location;
-  final DateTime? applyDeadline;
+enum UploadResultType {
+  success,
+  permissionDenied,
+  invalidFields,
+  unknownError;
+}
 
-  static bool isValidExcelRow(ExcelRow row) {
-    if (row.get(idColumn) == null) return false;
-    if (int.tryParse(row.get(idColumn)!) == null) return false;
-    if (row.get(titleColumn) == null) return false;
-    if (row.get(descriptionColumn) == null) return false;
-    if (row.get(typeColumn) == null) return false;
-    if (row.get(contactNameColumn) == null) return false;
-    if (row.get(contactEmailColumn) == null) return false;
-    if (row.get(beginPostingColumn) == null) return false;
-    if (DateTime.tryParse(row.get(beginPostingColumn)!) == null) return false;
-    if (row.get(endPostingColumn) == null) return false;
-    if (DateTime.tryParse(row.get(endPostingColumn)!) == null) return false;
-    return true;
-  }
+class UploadResult {
+  const UploadResult(this.type, this.snapshot);
+  final UploadResultType type;
+  final ExcelRow snapshot;
 
-  static UploadItem? fromExcelRow(ExcelRow row) {
-    return isValidExcelRow(row)
-        ? UploadItem(
-            id: int.parse(row.get(idColumn)!),
-            title: row.get(titleColumn)!,
-            description: row.get(descriptionColumn)!,
-            type: row.get(typeColumn)!,
-            contactName: row.get(contactNameColumn)!,
-            contactEmail: row.get(contactEmailColumn)!,
-            beginPosting: DateTime.parse(row.get(beginPostingColumn)!),
-            endPosting: DateTime.parse(row.get(endPostingColumn)!),
-            date: DateTime.tryParse(row.get(dateColumn) ?? ''),
-            time: row.get(timeColumn),
-            location: row.get(locationColumn),
-            applyDeadline:
-                DateTime.tryParse(row.get(applyDeadlineColumn) ?? ''),
-          )
-        : null;
-  }
+  factory UploadResult.success(ExcelRow snapshot) =>
+      UploadResult(UploadResultType.success, snapshot);
+  factory UploadResult.permissionDenied(ExcelRow snapshot) =>
+      UploadResult(UploadResultType.permissionDenied, snapshot);
+  factory UploadResult.invalidFields(ExcelRow snapshot) =>
+      UploadResult(UploadResultType.invalidFields, snapshot);
+  factory UploadResult.unknownError(ExcelRow snapshot) =>
+      UploadResult(UploadResultType.unknownError, snapshot);
+}
 
-  void uploadToFirestore() async {
-    final db = FirebaseFirestore.instance;
-    const collName = 'uploaderTest';
-    final data = <String, dynamic>{
-      'id': id,
-      'title': title,
-      'desc': description,
-      'eventType': type,
-      'contactName': contactName,
-      'contactEmail': contactEmail,
-      'beginPosting': beginPosting,
-      'endPosting': endPosting,
-      'date': date,
-      'time': time,
-      'location': location,
-      'applyDeadline': applyDeadline,
-    };
+Future<UploadResult> uploadToFirestore(ExcelRow row) async {
+  if (!_isValidExcelRow(row)) return UploadResult.invalidFields(row);
+  final data = _generateDocumentSnapshot(row);
+  final db = FirebaseFirestore.instance;
+  const collName = 'uploaderTest';
+  try {
     final match =
-        (await db.collection(collName).where('id', isEqualTo: id).get())
+        (await db.collection(collName).where('id', isEqualTo: data['id']).get())
             .docs
             .firstOrNull;
     if (match == null) {
-      db
-          .collection(collName)
-          .add(data)
-          .then((docRef) => debugPrint('Document added with ID: ${docRef.id}'));
+      await db.collection(collName).add(data);
+      return UploadResult.success(row);
     } else {
-      match.reference
-          .set(data)
-          .then((_) => debugPrint('Document updated with ID: ${match.id}'));
+      await match.reference.set(data);
+      return UploadResult.success(row);
     }
+  } catch (e) {
+    // e is just an `Object`, this is the only way to know it's a permission error
+    if (e.toString().contains('code=permission-denied')) {
+      return UploadResult.permissionDenied(row);
+    }
+    return UploadResult.unknownError(row);
   }
 }
+
+bool _isValidExcelRow(ExcelRow row) {
+  if (row.get(idColumn) == null) return false;
+  if (int.tryParse(row.get(idColumn)!) == null) return false;
+  if (row.get(titleColumn) == null) return false;
+  if (row.get(descriptionColumn) == null) return false;
+  if (row.get(typeColumn) == null) return false;
+  if (row.get(contactNameColumn) == null) return false;
+  if (row.get(contactEmailColumn) == null) return false;
+  if (row.get(beginPostingColumn) == null) return false;
+  if (DateTime.tryParse(row.get(beginPostingColumn)!) == null) return false;
+  if (row.get(endPostingColumn) == null) return false;
+  if (DateTime.tryParse(row.get(endPostingColumn)!) == null) return false;
+  return true;
+}
+
+Map<String, dynamic> _generateDocumentSnapshot(ExcelRow row) => {
+      'id': int.parse(row.get(idColumn)!),
+      'title': row.get(titleColumn)!,
+      'description': row.get(descriptionColumn)!,
+      'type': row.get(typeColumn)!,
+      'contactName': row.get(contactNameColumn)!,
+      'contactEmail': row.get(contactEmailColumn)!,
+      'beginPosting': DateTime.parse(row.get(beginPostingColumn)!),
+      'endPosting': DateTime.parse(row.get(endPostingColumn)!),
+      'date': DateTime.tryParse(row.get(dateColumn) ?? ''),
+      'time': row.get(timeColumn),
+      'location': row.get(locationColumn),
+      'applyDeadline': DateTime.tryParse(row.get(applyDeadlineColumn) ?? ''),
+    };
