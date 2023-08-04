d76ac96033cf47173b266deea37637f3fde3c401
reidst <reidst@hendrix.edu>
2023-08-04 15:30:31 -0500

    Added a "delete all old items" button

--- lib/firebase/download.dart
+++ lib/firebase/download.dart
@@ -52,3 +52,34 @@ String? _formatTimestamp(dynamic ts) {
   ][dt.month];
   return '${dt.year} $month ${dt.day}';
 }
+
+DateTime? undoFormatDate(String? field) {
+  if (field == null) return null;
+  final formattedString = field
+      // replace month names with numbers
+      .replaceFirst('Jan', '01')
+      .replaceFirst('Feb', '02')
+      .replaceFirst('Mar', '03')
+      .replaceFirst('Apr', '04')
+      .replaceFirst('May', '05')
+      .replaceFirst('Jun', '06')
+      .replaceFirst('Jul', '07')
+      .replaceFirst('Aug', '08')
+      .replaceFirst('Sep', '09')
+      .replaceFirst('Oct', '10')
+      .replaceFirst('Nov', '11')
+      .replaceFirst('Dec', '12')
+      // left-pad single-digit day values with a 0
+      .replaceFirst(RegExp(r' 1$'), '01')
+      .replaceFirst(RegExp(r' 2$'), '02')
+      .replaceFirst(RegExp(r' 3$'), '03')
+      .replaceFirst(RegExp(r' 4$'), '04')
+      .replaceFirst(RegExp(r' 5$'), '05')
+      .replaceFirst(RegExp(r' 6$'), '06')
+      .replaceFirst(RegExp(r' 7$'), '07')
+      .replaceFirst(RegExp(r' 8$'), '08')
+      .replaceFirst(RegExp(r' 9$'), '09')
+      // replace spaces with hyphens
+      .replaceAll(' ', '-');
+  return DateTime.tryParse(formattedString);
+}
--- lib/screens/view_databse.dart
+++ lib/screens/view_databse.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:hendrix_today_uploader/firebase/download.dart';
 import 'package:hendrix_today_uploader/firebase/constants.dart';
+import 'package:hendrix_today_uploader/firebase/upload.dart';
 import 'package:hendrix_today_uploader/widgets/table_item.dart';
 import 'package:hendrix_today_uploader/widgets/table_scroller.dart';
 
@@ -14,6 +15,7 @@ class DatabaseViewScreen extends StatefulWidget {
 
 class _DatabaseViewScreenState extends State<DatabaseViewScreen> {
   List<FirestoreRow>? _items;
+  bool _deleteInProgress = false;
 
   @override
   void initState() {
@@ -23,9 +25,101 @@ class _DatabaseViewScreenState extends State<DatabaseViewScreen> {
 
   void _getFirestoreContents() async {
     final items = await getFirestoreContents();
-    setState(() {
-      _items = items;
-    });
+    if (mounted) {
+      setState(() {
+        _items = items;
+      });
+    }
+  }
+
+  void _tryDeleteOldEvents() async {
+    if (_deleteInProgress) return;
+    if (_items == null) return;
+    if (mounted) {
+      setState(() {
+        _deleteInProgress = true;
+      });
+    }
+    final today = DateTime.now();
+    final oldItems = _items!
+        .where((row) => undoFormatDate(row[7])?.isBefore(today) ?? false);
+    final deleteResults = <UploadResult>[];
+    for (final item in oldItems) {
+      deleteResults.add(await deleteFromFirestore(item));
+      if (deleteResults.last.type == UploadResultType.permissionDenied) break;
+    }
+    if (mounted) {
+      setState(() {
+        _deleteInProgress = false;
+      });
+    }
+    _displayDeleteResults(deleteResults);
+    _getFirestoreContents();
+  }
+
+  /// Displays [SnackBar] messages for each [UploadResultType] in [results].
+  ///
+  /// This method lives outside the [_tryDeleteOldEvents] method because
+  /// [BuildContext]s cannot be used in `async` methods.
+  void _displayDeleteResults(List<UploadResult> results) {
+    if (results
+        .any((result) => result.type == UploadResultType.permissionDenied)) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text(
+            'You do not have delete permission. Please sign in or contact the '
+            'maintainer of this upload tool.'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+      return;
+    }
+    final invalidFields = results
+        .where((result) => result.type == UploadResultType.invalidFields);
+    if (invalidFields.isNotEmpty) {
+      String displayIDs = invalidFields
+          .map((result) => result.snapshot[idField.index] ?? '[missing ID]')
+          .join(', ');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+            'The following rows (by ID) are improperly formatted or missing '
+            'required data: $displayIDs'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    }
+    final idsNotPresent =
+        results.where((result) => result.type == UploadResultType.idNotPresent);
+    if (idsNotPresent.isNotEmpty) {
+      String displayIDs = idsNotPresent
+          .map((result) => result.snapshot[idField.index])
+          .join(', ');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+            'The following rows (by ID) could not be deleted because their IDs '
+            'are not present in the database: $displayIDs'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    }
+    final unknownErrors =
+        results.where((result) => result.type == UploadResultType.unknownError);
+    if (unknownErrors.isNotEmpty) {
+      String displayIDs = unknownErrors
+          .map((result) => result.snapshot[idField.index])
+          .join(', ');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+            'An unknown error occurred trying to delete the following rows (by '
+            'ID): $displayIDs'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+    }
+    final successfulDeletes = results
+        .where((result) => result.type == UploadResultType.successfulDelete);
+    if (successfulDeletes.isNotEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Successfully deleted ${successfulDeletes.length} '
+            'item${successfulDeletes.length != 1 ? 's' : ''}!'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ));
+    }
   }
 
   @override
@@ -43,9 +137,19 @@ class _DatabaseViewScreenState extends State<DatabaseViewScreen> {
           children: [
             if (_items != null)
               Padding(
-                padding: const EdgeInsets.all(8),
-                child:
+                padding: const EdgeInsets.fromLTRB(8, 4, 8, 10),
+                child: Row(
+                  children: [
                     Text('There are ${_items!.length} items in the database.'),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: _tryDeleteOldEvents,
+                      child: _deleteInProgress
+                          ? const Text('Deleting...')
+                          : const Text('Delete all old events'),
+                    ),
+                  ],
+                ),
               ),
             if (_items == null)
               const Center(child: Text('Loading...'))
